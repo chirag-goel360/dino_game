c6c3ef293e210597be8a994d8fdc79666468cf64
Chirag Goel <61325696+chirag-goel360@users.noreply.github.com>
2021-04-22 15:37:59 +0530

    Setting Background

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,9 +1,16 @@
 import 'package:flame/components/animation_component.dart';
+import 'package:flame/components/parallax_component.dart';
 import 'package:flame/game/base_game.dart';
 import 'package:flame/spritesheet.dart';
+import 'package:flutter/cupertino.dart';
+
+const double groundHeight = 32;
+const double dinoTopBottomSpacing = 10;
+const int numberOfTilesAlongWidth = 10;
 
 class DinoGame extends BaseGame{
   AnimationComponent _dino;
+  ParallaxComponent _parallaxComponent;
 
   DinoGame(){
     _dino = AnimationComponent.empty();
@@ -22,10 +29,28 @@ class DinoGame extends BaseGame{
     final idleAnimation = spriteSheet.createAnimation(0,from: 0,to: 3,stepTime: 0.1);
     final runAnimation = spriteSheet.createAnimation(0,from: 4,to: 10,stepTime: 0.1);
     _dino.animation = runAnimation;
-    _dino.height = 80;
-    _dino.width = 80;
-    _dino.x = 80;
-    _dino.y = 200;
+    _parallaxComponent = ParallaxComponent(
+      [
+        ParallaxImage('parallax/plx-1.png'),
+        ParallaxImage('parallax/plx-2.png'),
+        ParallaxImage('parallax/plx-3.png'),
+        ParallaxImage('parallax/plx-4.png'),
+        ParallaxImage('parallax/plx-5.png'),
+        ParallaxImage('parallax/plx-6.png',fill: LayerFill.none),
+      ],
+      baseSpeed: Offset(100,0),
+      layerDelta: Offset(20,0),
+    );
+    add(_parallaxComponent);
     add(_dino);
-  }  
+  }
+
+  @override
+  void resize(Size size) {
+    super.resize(size);
+    debugPrint(size.toString());
+    _dino.height = _dino.width = size.width/numberOfTilesAlongWidth;
+    _dino.x = _dino.width;
+    _dino.y = size.height - groundHeight - _dino.height + dinoTopBottomSpacing;
+  }
 }
